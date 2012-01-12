47c420069645c068750e39e3d9bac239a1c44cd0
NothingDotSort <Nothing.sort@Gmail.com>
2012-01-12 17:12:13 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b7abfd1c8f481a454e5404813347200f810fc8e1e1df3aadf89d5fa7971e0ed6da619030f3f224bfcb9fa3055e221a2f849a8e96a9f6105fccaa4933d415d07d'
